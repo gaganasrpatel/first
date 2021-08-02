317199674f0962d57f8b268d36958952d74ca058
Gagana Sr -X (gasr - WIPRO LIMITED at Cisco) <gasr@partnet.cisco.com>
2021-08-02 11:30:50 +0530

    modified

--- three.py
+++ three.py
@@ -1,6 +1,19 @@
-def shout(text):
-    return text.upper()
-print(shout("hello"))
+import time
+from locust import HttpUser, task, between
 
-yell=shout
-print(yell("hi"))
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/hello")
+        self.client.get("/world")
+
+    @task(3)
+    def view_items(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            time.sleep(1)
+
+    def on_start(self):
+        self.client.post("/login", json={"username":"foo", "password":"bar"})
